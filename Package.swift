51e3273561537b8523ebc40b407371b440748f38
Nic Jackson <jackson.nic@gmail.com>
2017-07-06 07:18:57 +0100

    Updated packages

--- Package.swift
+++ Package.swift
@@ -13,6 +13,6 @@ let package = Package(
 
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 7),
-        .Package(url: "https://github.com/nicholasjackson/kitura-http-test.git", "0.2.0")
+        .Package(url: "https://github.com/nicholasjackson/kitura-http-test.git", majorVersion:0, minor: 2)
     ]
 )
