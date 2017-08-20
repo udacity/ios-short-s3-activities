51a3c1f8a7de468796dc400375afad7df5460191
Nic Jackson <jackson.nic@gmail.com>
2017-08-20 13:50:21 +0100

    Updated to latest mysql driver

--- Package.swift
+++ Package.swift
@@ -7,13 +7,13 @@ let package = Package(
 
     targets: [
         Target(name: "ActivitiesService"),
-        Target(name: "ActivitiesServer", dependencies: ["ActivitiesService"]),
+        Target(name: "ActivitiesServer", dependencies: ["ActivitiesService"])
     ],
 
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 7),
         .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 7),
-        .Package(url: "https://github.com/nicholasjackson/swift-mysql.git", majorVersion: 1, minor: 3)
+        .Package(url: "https://github.com/nicholasjackson/swift-mysql.git", majorVersion: 1, minor: 4)
     ]
 )
 
