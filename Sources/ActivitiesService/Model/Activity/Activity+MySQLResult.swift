9d53abb25da9d52dc3aed11b64385ffce888adbd
Jarrod Parkes <parkesfjarrod@gmail.com>
2017-08-22 10:02:32 -0500

    simplify toActivities

--- Sources/ActivitiesService/Model/Activity/Activity+MySQLResult.swift
+++ Sources/ActivitiesService/Model/Activity/Activity+MySQLResult.swift
@@ -7,50 +7,32 @@ import Foundation
 public extension MySQLResultProtocol {
 
     public func toActivities() -> [Activity] {
-                
+
         var activities = [Activity]()
 
         while case let row? = self.nextResult() {
 
             var activity = Activity()
 
-            if let id = row["id"] as? Int {
-                activity.id = Int(id)
-            }
-
-            if let minParticipants = row["min_participants"] as? Int {
-                activity.minParticipants = Int(minParticipants)
-            }
-
-            if let maxParticipants = row["max_participants"] as? Int {
-                activity.maxParticipants = Int(maxParticipants)
-            }
+            activity.id = row["id"] as? Int
+            activity.minParticipants = row["min_participants"] as? Int
+            activity.maxParticipants = row["max_participants"] as? Int
+            activity.name = row["name"] as? String
+            activity.emoji = row["emoji"] as? String
+            activity.genre = row["genre"] as? String
+            activity.description = row["description"] as? String
 
             let dateFormatter = DateFormatter()
             dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
 
-            if let createdAtString = row["created_at"] as? String, let createdAt = dateFormatter.date(from: createdAtString) {
-                activity.createdAt = createdAt
+            if let createdAtString = row["created_at"] as? String,
+               let createdAt = dateFormatter.date(from: createdAtString) {
+                   activity.createdAt = createdAt
             }
 
-            if let updatedAtString = row["updated_at"] as? String, let updatedAt = dateFormatter.date(from: updatedAtString) {
-                activity.updatedAt = updatedAt
-            }
-
-            if let name = row["name"] as? String {
-                activity.name = name
-            }
-
-            if let emoji = row["emoji"] as? String {
-                activity.emoji = emoji
-            }
-
-            if let genre = row["genre"] as? String {
-                activity.genre = genre
-            }
-
-            if let description = row["description"] as? String {
-                activity.description = description
+            if let updatedAtString = row["updated_at"] as? String,
+               let updatedAt = dateFormatter.date(from: updatedAtString) {
+                   activity.updatedAt = updatedAt
             }
 
             activities.append(activity)
