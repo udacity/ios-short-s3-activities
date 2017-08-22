04897133792700b1463ea898252f02b728985e00
Jarrod Parkes <parkesfjarrod@gmail.com>
2017-08-22 09:55:55 -0500

    refactor toActivities

--- Sources/ActivitiesService/Model/Activity/Activity+MySQLResult.swift
+++ Sources/ActivitiesService/Model/Activity/Activity+MySQLResult.swift
@@ -14,21 +14,17 @@ public extension MySQLResultProtocol {
 
             var activity = Activity()
 
-            if let id = row["id"] as? Int {
-                activity.id = Int(id)
-            }
-
-            if let minParticipants = row["min_participants"] as? Int {
-                activity.minParticipants = Int(minParticipants)
-            }
-
-            if let maxParticipants = row["max_participants"] as? Int {
-                activity.maxParticipants = Int(maxParticipants)
-            }
+            activity.id = row["id"] as? Int
+            activity.minParticipants = row["min_participants"] as? Int
+            activity.maxParticipants = row["max_participants"] as? Int
+            activity.name = row["name"] as? String
+            activity.emoji = row["emoji"] as? String
+            activity.genre = row["genre"] as? String
+            activity.description = row["description"] as? String
 
             let dateFormatter = DateFormatter()
             dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
-
+            
             if let createdAtString = row["created_at"] as? String,
                let createdAt = dateFormatter.date(from: createdAtString) {
                    activity.createdAt = createdAt
@@ -37,22 +33,6 @@ public extension MySQLResultProtocol {
             if let updatedAtString = row["updated_at"] as? String,
                let updatedAt = dateFormatter.date(from: updatedAtString) {
                    activity.updatedAt = updatedAt
-            }
-
-            if let name = row["name"] as? String {
-                activity.name = name
-            }
-
-            if let emoji = row["emoji"] as? String {
-                activity.emoji = emoji
-            }
-
-            if let genre = row["genre"] as? String {
-                activity.genre = genre
-            }
-
-            if let description = row["description"] as? String {
-                activity.description = description
             }
 
             activities.append(activity)
